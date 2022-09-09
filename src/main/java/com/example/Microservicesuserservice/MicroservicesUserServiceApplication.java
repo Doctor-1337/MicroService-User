ee0e5c44009666978a7700a3a54205b43a8eff74
Mohd Nabeel <doctor@Mohds-MBP.Dlink>
2022-09-09 16:43:41 +0530

    first commit

---
+++ src/main/java/com/example/Microservicesuserservice/MicroservicesUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservicesuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesUserServiceApplication.class, args);
+	}
+
+}
